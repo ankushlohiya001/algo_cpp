f96ed48fa8b12d0ab5f8457ef7d45b774592edf1
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-01-16 00:27:33 +0530

    tested basic working :))

--- src/smart_ptr/unique_ptr.hpp
+++ src/smart_ptr/unique_ptr.hpp
@@ -1,1 +1,28 @@
 #pragma once
+
+template <class T> class unique_ptr {
+  T *data_ptr;
+
+public:
+  unique_ptr() {}
+  unique_ptr(T *ptr) : data_ptr(ptr) {}
+
+  ~unique_ptr() {
+    if (data_ptr != nullptr) {
+      // to avoid deleting empty ptr
+      delete data_ptr;
+    }
+  }
+
+  void operator=(unique_ptr<T> another) {
+    // workaround to avoid copying pointer, which would otherwise
+    // resulted in dual pointer deleting, ie. deleting a ptr which
+    // was deleted already
+    data_ptr = another.data_ptr;
+    another.data_ptr = nullptr;
+  }
+};
+
+template <class T, class... Args> unique_ptr<T> make_unique(Args &&...args) {
+  return unique_ptr(new T(args...));
+}
--- test/smart_ptr_test.cpp
+++ test/smart_ptr_test.cpp
@@ -1,3 +1,21 @@
+#include "smart_ptr.hpp"
 #include <iostream>
 
-int main() { return 0; }
+struct Test {
+public:
+  Test() { std::cout << "created!!\n"; }
+
+  ~Test() { std::cout << "deleted!!\n"; }
+};
+
+int main() {
+  {
+    unique_ptr<Test> t1;
+    std::cout << "block started!!\n";
+    t1 = unique_ptr<Test>(new Test);
+    std::cout << "block ended!!\n";
+  }
+
+  std::cout << "prog ended\n";
+  return 0;
+}
