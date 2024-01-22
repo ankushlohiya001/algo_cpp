4102f75b4779f5aa0d92d58f28af923946009ca0
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-01-22 17:49:44 +0530

    adding description to selection sort

--- src/sorting/selection_sort.hpp
+++ src/sorting/selection_sort.hpp
@@ -1,5 +1,17 @@
 #pragma once
 
+/*\
+ * selection sorting works by following manner:
+ * iterate over positions
+ * for each positions, selecting the minimum/maximum
+ * element and replace min/max by current position.
+ * thus sorting list by following complexities
+ *
+ * best case: n^2
+ * avg case: n^2
+ * worst case: n^2
+ * */
+
 template <class T> void selection_sort(T *arr, long length) {
   for (long i = 0; i < length; i++) {
     long min_ind = i;
