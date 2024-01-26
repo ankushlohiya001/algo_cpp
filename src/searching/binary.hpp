2cd1c3035b4de3193d416015fab22147f1dcebd8
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-01-26 19:50:26 +0530

    added function to check possible position in binary search

--- src/searching/binary.hpp
+++ src/searching/binary.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+template <class T> int sorted_position(T *arr, T what, int length);
+
 /*
  * binary search based on binary tree,
  * so, to perform a binary_search, we need to have
@@ -7,6 +9,25 @@
  * */
 template <class T> int binary_search(T *arr, T what, int length) {
   // expecting sorted arr, to work correctly
+  int position = sorted_position(arr, what, length);
+  // we basically says, if possible position of `what` in arr,
+  // actually contains it, means it's real ):
+  // and in other case, possiblity is just possiblity :))
+  if (position < length && arr[position] == what) {
+    return position;
+  } else {
+    return -1;
+  }
+}
+
+/*
+ * as binary_search tells whether index if present
+ * and -1 incase of not found,
+ * but we can slightly modify this behaviour to so that,
+ * instead of return -1 incase of not found, it returns
+ * possible index, where it could've been present. :)
+ * */
+template <class T> int sorted_position(T *arr, T what, int length) {
   int start = 0;
   int end = length;
   while (start < end) {
@@ -20,5 +41,5 @@ template <class T> int binary_search(T *arr, T what, int length) {
     }
   }
 
-  return -1;
+  return start;
 }
--- test/searching_test.cpp
+++ test/searching_test.cpp
@@ -5,7 +5,7 @@
 int main() {
   int *items = new int[]{1, 5, 34, 23, 123, 45};
   insertion_sort(items, 6); // 1 5 23 34 45 123
-  int index = binary_search(items, 123, 6);
+  int index = binary_search(items, 37, 6);
   std::cout << index << std::endl;
   return 0;
 }
