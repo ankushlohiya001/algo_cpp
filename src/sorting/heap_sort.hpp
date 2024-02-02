a689c57a45127e3003898ffd9e5da325eeab5fba
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-02-02 22:04:13 +0530

    added some description :))

--- src/sorting/heap_sort.hpp
+++ src/sorting/heap_sort.hpp
@@ -35,14 +35,27 @@ template <class T> void move_down(T *arr, int index, int end) {
     }
   }
 }
+/*
+ * heap sort works by first making array into binary heap,
+ * binary heap conversion works by following way,
+ */
 
 template <class T> void heap_sort(T *arr, int length) {
   int last_leaf_idx = (length + (length & 1)) / 2;
   int crnt = last_leaf_idx;
+  /*
+   * we move_down nodes one by one, we start from last(leaving leaf node).
+   * this is important because there's always half count of leaf nodes.
+   * */
   while (crnt >= 0) {
     move_down(arr, crnt--, length);
   }
 
+  /*
+   * we then swap first node to last node, and move down first node
+   * to ending leaving last node,
+   * length decreases in each iteration.
+   * */
   for (int i = length - 1; i > 0; i--) {
     swap(arr[0], arr[i]);
     move_down(arr, 0, i);
