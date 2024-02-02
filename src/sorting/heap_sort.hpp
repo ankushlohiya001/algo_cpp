02756d4e679a6ada2026a5a1df68ac59b9949acb
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-02-02 21:47:43 +0530

    added heap_sort algo finally :))

--- src/sorting/heap_sort.hpp
+++ src/sorting/heap_sort.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
-template <class T> void move_down(T *arr, int begin, int end) {
-  int crnt_idx = begin;
+template <class T> void swap(T &ref_a, T &ref_b) {
+  T tmp = ref_a;
+  ref_a = ref_b;
+  ref_b = tmp;
+}
+
+template <class T> void move_down(T *arr, int index, int end) {
+  int crnt_idx = index;
   while (true) {
     int a_idx = crnt_idx * 2 + 1; // left child index
     if (a_idx >= end)
@@ -13,11 +19,11 @@ template <class T> void move_down(T *arr, int begin, int end) {
     }
     int rare = crnt_idx;
 
-    if (arr[a_idx] < arr[rare]) {
+    if (arr[a_idx] > arr[rare]) {
       rare = a_idx;
     }
 
-    if (arr[b_idx] < arr[rare]) {
+    if (arr[b_idx] > arr[rare]) {
       rare = b_idx;
     }
 
@@ -32,4 +38,13 @@ template <class T> void move_down(T *arr, int begin, int end) {
 
 template <class T> void heap_sort(T *arr, int length) {
   int last_leaf_idx = (length + (length & 1)) / 2;
+  int crnt = last_leaf_idx;
+  while (crnt >= 0) {
+    move_down(arr, crnt--, length);
+  }
+
+  for (int i = length - 1; i > 0; i--) {
+    swap(arr[0], arr[i]);
+    move_down(arr, 0, i);
+  }
 }
--- test/sorting_test.cpp
+++ test/sorting_test.cpp
@@ -1,13 +1,15 @@
-#include "insertion_sort.hpp"
 /* #include "sorting.hpp" */
+#include "heap_sort.hpp"
 #include <iostream>
 
 int main() {
-  long *arr = new long[]{5, 2, 6, 3, 2, 0};
+  long arr[] = {1, 3, 2, 0, 4, 3, 5, 1};
 
-  insertion_sort<long>(arr, 6);
+  int len = sizeof(arr) / sizeof(long);
 
-  for (int i = 0; i < 6; i++) {
+  heap_sort<long>(arr, len);
+
+  for (int i = 0; i < len; i++) {
     std::cout << arr[i] << ", ";
   }
   std::cout << "\n";
