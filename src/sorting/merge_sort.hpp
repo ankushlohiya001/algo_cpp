aef108a845417049f57db88cb49f399e96fc3169
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-02-03 22:39:56 +0530

    added code for merge sort, not working :(

--- src/sorting/merge_sort.hpp
+++ src/sorting/merge_sort.hpp
@@ -1,5 +1,13 @@
 #pragma once
 
+template <class T> void swap(T &ref_a, T &ref_b) {
+  T tmp = ref_a;
+  ref_a = ref_b;
+  ref_b = tmp;
+}
+
+template <class T> void divider_merger(T *arr, int from, int to);
+
 /*
  * merge sort works by following way
  * 1. dividing the elements into half until less than 2 remains(in each pair).
@@ -12,4 +20,57 @@
  *  try to do some tail call optimizations.
  *  to reduce extra cost.
  * */
-template <class T> void merge_sort(T *arr, int length) {}
+template <class T> void merge_sort(T *arr, int length) {
+  divider_merger(arr, 0, length);
+}
+
+/*
+ * this merging function is used to merge two sorted array,
+ * to form new sorted array, it works in following manner. :D
+ *
+ * initialize two pointer for each array's front(first element),
+ *
+ * Note:
+ *  Here' two array is emulated using subArray(array's sub part),
+ *  specified by indexed ranges.
+ * */
+template <class T> void merge(T *arr, int from, int to) {
+  int len = (to - from);
+  int mid = from + len / 2;
+
+  int i = from;
+
+  while (i < mid) {
+    if (arr[i] > arr[mid]) {
+      swap(arr[i], arr[mid]);
+
+      int j = mid;
+      while (j + 1 < to && arr[j] > arr[j + 1]) {
+        swap(arr[j], arr[j + 1]);
+        j++;
+      }
+    }
+    i++;
+  }
+}
+
+template <class T> void divider_merger(T *arr, int from, int to) {
+  int len = to - from;
+  if (len < 2) {
+    return;
+  } else if (len == 2) {
+    // sort two numbers,
+    if (arr[from] > arr[to]) {
+      swap(arr[from], arr[to]);
+    }
+  } else {
+
+    // dividing and merging until they not reach count less than 3,
+    int mid = from + len / 2;
+    divider_merger(arr, from, mid); // first half
+
+    divider_merger(arr, mid, len); // second half
+
+    merge(arr, from, len);
+  }
+}
--- test/sorting_test.cpp
+++ test/sorting_test.cpp
@@ -1,5 +1,5 @@
 /* #include "sorting.hpp" */
-#include "heap_sort.hpp"
+#include "merge_sort.hpp"
 #include <iostream>
 
 int main() {
@@ -7,7 +7,7 @@ int main() {
 
   int len = sizeof(arr) / sizeof(long);
 
-  heap_sort<long>(arr, len);
+  merge_sort<long>(arr, len);
 
   for (int i = 0; i < len; i++) {
     std::cout << arr[i] << ", ";
@@ -15,3 +15,14 @@ int main() {
   std::cout << "\n";
   return 0;
 }
+/*
+
+1 3 2, 0 4 3 5
+
+1 3, 2 0, 4 3, 5 1
+1 3, 0 2, 3 4, 1 5
+0 1 2 3, 1 3 4 5
+
+0 1 1 2 3 3 4 5
+
+*/
