a1333f4f87ef2749f4c33cb638ede546c4c1baf1
ankushlohiya001 <ankushlohiya2000@gmail.com>
2024-02-23 15:35:49 +0530

    added: few maths' functions :C

--- src/math/math.hpp
+++ src/math/math.hpp
@@ -1,1 +1,18 @@
 #pragma once
+
+void div_mod(int de, int no, int *div, int *mod) {
+  *div = de / no;
+  *mod = de - (*div) * no;
+}
+
+int hcf(int a, int b) {
+  if (b == 0) {
+    return a;
+  } else {
+    return hcf(b, a % b);
+  }
+}
+
+inline int gcd(int a, int b) { return hcf(a, b); }
+
+int lcm(int a, int b) { return (a / hcf(a, b)) * b; }
---
+++ test/math_test.cpp
@@ -0,0 +1,10 @@
+#include "math.hpp"
+#include <iostream>
+
+int main() {
+  std::cout << "--------------------\n";
+  int val = hcf(16, 2390);
+  std::cout << val;
+  std::cout << "\n--------------------\n";
+  return 0;
+}
